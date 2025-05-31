23f6cb1b0649b34a2a078eacaf2d0349555b2851
Disty0 <disty@disty.xyz>
2025-05-31 22:52:14 +0300

    update pixiv tag

--- create-tags-from-json.py
+++ create-tags-from-json.py
@@ -265,9 +265,9 @@ def get_tags_from_json(json_path: str, image_path: str) -> str:
     if pixiv_tags:
         for raw_pixiv_tag in pixiv_tags:
             if raw_pixiv_tag and raw_pixiv_tag not in pixiv_tag_blacklist:
+                if raw_pixiv_tag.lower().endswith("+_bookmarks"):
+                    raw_pixiv_tag = raw_pixiv_tag.rsplit("_", maxsplit=2)[0]
                 pixiv_tag = tag_dict.get(raw_pixiv_tag, raw_pixiv_tag.replace(" ", "_").lower())
-                if pixiv_tag.endswith("+_bookmarks"):
-                    pixiv_tag = pixiv_tag.rsplit("_", maxsplit=2)[0]
                 if pixiv_tag.isascii():
                     pixiv_tag_category = tag_categories.get(pixiv_tag, 0)
                     if pixiv_tag_category == 0 and pixiv_tag not in split_general_tags:
